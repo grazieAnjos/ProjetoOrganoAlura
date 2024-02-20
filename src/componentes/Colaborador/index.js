0466c7d19ebc1fdc20c3d1831fbd6388aa11aeb7
grazieAnjos <grazieleanjosl@gmail.com>
2024-02-20 14:04:40 -0300

    Adição de um formulario para adição de times no organo e a funcionalidade de excluir e favoritar os colaboradores.

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,13 +1,36 @@
+import { IoMdCloseCircle } from "react-icons/io";
+import { FaHeart, FaRegHeart  } from "react-icons/fa";
 import './colaborador.css'
 
-const Colaborador = ({ colaborador, corDeFundo }) => {
+
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar, aoFavoritar}) => {
+    function favoritar(){
+        aoFavoritar(colaborador.id);
+    }
+
+    const propsFavorito = {
+        size:25,
+        onclick: favoritar
+    }
+
     return (<div className="colaborador">
+        <IoMdCloseCircle
+            size={25}
+            className="deletar"
+            onClick={() => aoDeletar(colaborador.id)}
+        />
         <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
             <img src={colaborador.imagem} alt={colaborador.nome} />
         </div>
         <div className="rodape">
             <h4>{colaborador.nome}</h4>
             <h5>{colaborador.cargo}</h5>
+            <div className='favoritar'>
+                {colaborador.favorito 
+                    ? <FaHeart onClick={favoritar} color='#ff0000'/> 
+                    : <FaRegHeart onClick={favoritar}/>
+                }
+            </div>
         </div>
     </div>)
 }
